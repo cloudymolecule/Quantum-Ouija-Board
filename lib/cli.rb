1ca72f5e004edd067d90b361577a9ad0f5d73448
Cloudymolecule <baires82@gmail.com>
2021-07-08 17:03:10 -0400

    Sanitize input

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,16 @@
 class CLI
     def start
-        puts 'hi'
+        puts "WELCOME TO QUANTUM OUIJA BOARD"
+        puts "Ask a question..."
+        input = gets.strip
+        proper_input = check_question_input(input)
+        puts proper_input
+    end
+
+    private
+
+    def check_question_input(input)
+        input = input.gsub(/\W/,' ') + '?'
+        input
     end
 end
