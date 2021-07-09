94cefeb76200fb3b933b7c608a7f4c358e541852
Cloudymolecule <baires82@gmail.com>
2021-07-08 21:23:07 -0400

    Update input sanitation

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,14 +3,22 @@ class CLI
         puts "WELCOME TO QUANTUM OUIJA BOARD"
         puts "Ask a question..."
         input = gets.strip
+        until input != ''
+            puts "Come on, just ask a little question"
+            input = gets.strip
+        end
         proper_input = check_question_input(input)
-        puts proper_input
+        puts 'Is this your question?: ' + proper_input
+        puts 'Y/N'
+        question_yn = gets.strip.downcase
+        until question_yn == 'y' || question_yn == 'n'
+            puts 'it works'
+        end
     end
 
     private
 
-    def check_question_input(input)
-        input = input.gsub(/\W/,' ') + '?'
-        input
+    def check_question_input(input) # => Sanitize input
+        return input = input.gsub(/\W/,' ') + '?'
     end
 end
